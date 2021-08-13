c0bc16a0ae6a09b9882129829cae3cb061fb9595
phi16 <phi16@example.com>
2021-08-12 22:13:50 -0600

    added vehicles and teleport

--- src/entities.rs
+++ src/entities.rs
@@ -15,6 +15,12 @@ impl Powerup {
                            encode::base32_encode(self.x), 
                            encode::base32_encode(self.y * -1)
             );
+        } else if self.rotation == 1000 {
+            return format!("V {} {} {} a,",
+                            encode::base32_encode(self.x),
+                            encode::base32_encode(self.y * -1),
+                            self.powerup_type.to_string()
+            );
         }
         
         return format!("{} {} {} {},", 
@@ -23,6 +29,25 @@ impl Powerup {
                        encode::base32_encode(self.y * -1), 
                        encode::base32_encode(self.rotation)
         );
+    }
+}
+
+
+pub struct Teleport {
+    pub x1: i32,
+    pub y1: i32,
+    pub x2: i32,
+    pub y2: i32,
+}
+
+impl Teleport {
+    pub fn encode(self) -> String {
+        format!("W {} {} {} {}",
+                encode::base32_encode(self.x1),
+                encode::base32_encode(self.y1 * -1),
+                encode::base32_encode(self.x2),
+                encode::base32_encode(self.y2 * -1)
+        )
     }
 }
 
--- src/lib.rs
+++ src/lib.rs
@@ -14,6 +14,7 @@ pub struct Track {
 }
 
 impl Track {
+    // Lines
     pub fn insert_line(&mut self, x1: i32, y1: i32, x2: i32, y2: i32, line_type: char) {
         if line_type == 'p' {
             self.physical.push(entities::Line{ line_type, x1, y1, x2, y2 }.encode());
@@ -22,6 +23,7 @@ impl Track {
         }
     }
     
+    // Powerups
     pub fn insert_check(&mut self, x: i32, y: i32) {
         self.powerups += &entities::Powerup { powerup_type: 'C', x, y, rotation: 999 }.encode();
     }
@@ -46,6 +48,32 @@ impl Track {
         self.powerups += &entities::Powerup { powerup_type: 'B', x, y, rotation: rot }.encode();
     }
 
+    pub fn insert_anti_gravity(&mut self, x: i32, y: i32, rot: i32) {
+        self.powerups += &entities::Powerup { powerup_type: 'A', x, y, rotation: 999 }.encode();
+    }
+
+    pub fn insert_teleport(&mut self, x1: i32, y1: i32, x2: i32, y2: i32) {
+        self.powerups += &entities::Teleport { x1, y1, x2, y2 }.encode();
+    }
+
+    // Vehicles
+    pub fn insert_helicopter(&mut self, x: i32, y: i32) {
+        self.powerups += &entities::Powerup { powerup_type: '1', x, y, rotation: 1000 }.encode();
+    }
+
+    pub fn insert_truck(&mut self, x: i32, y: i32) {
+        self.powerups += &entities::Powerup { powerup_type: '2', x, y, rotation: 1000 }.encode();
+    }
+
+    pub fn insert_balloon(&mut self, x: i32, y: i32) {
+        self.powerups += &entities::Powerup { powerup_type: '3', x, y, rotation: 1000 }.encode();
+    }
+
+    pub fn insert_blob(&mut self, x: i32, y: i32) {
+        self.powerups += &entities::Powerup { powerup_type: '4', x, y, rotation: 1000 }.encode();
+    }
+    
+    // Track code
     pub fn generate_code(&mut self) -> String {
         for physical_line in &self.physical {
             self.trackdata += physical_line;
