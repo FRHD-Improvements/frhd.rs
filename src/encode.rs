9d9a6729cd53151daa272e6e92cfbf45b1bd1bc5
gaetgu <gabriel.ethan@gmail.com>
2022-06-21 19:00:14 -0600

    Fixed a stupid error causing track gens to have random glitches

--- src/encode.rs
+++ src/encode.rs
@@ -1,24 +1,24 @@
 pub fn base32_encode(target: i32) -> String {
-    let alphabet: Vec<char> = vec!['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a',
-                                    'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l',
-                                    'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v'];
+    let alphabet: Vec<char> = vec![
+        '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h',
+        'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v',
+    ];
 
     let mut number = target.abs();
     let mut final_data = String::new();
-    let mut i: i32;
+
     while number != 0 {
-        i = number % 32;
-        number = (number - 1) / 32;
+        let i = number % 32;
+        number = (number - i) / 32;
         final_data = format!("{}{}", alphabet[i as usize].to_string(), final_data);
     }
     if target < 0 {
         final_data = format!("{}{}", "-", final_data);
     }
 
-    if final_data != "".to_string() {
+    if !final_data.is_empty() {
         return final_data;
     } else {
         return alphabet[0 as usize].to_string();
     }
 }
-
--- src/lib.rs
+++ src/lib.rs
@@ -14,16 +14,34 @@ impl Track {
     // Lines
     pub fn insert_line(&mut self, x1: i32, y1: i32, x2: i32, y2: i32, line_type: char) {
         if line_type == 'p' {
-            self.physical.push(entities::Line{ line_type, x1, y1, x2, y2 }.encode());
+            self.physical.push(
+                entities::Line {
+                    line_type,
+                    x1,
+                    y1,
+                    x2,
+                    y2,
+                }
+                .encode(),
+            );
         } else {
-            self.scenery.push(entities::Line{ line_type, x1, y1, x2, y2 }.encode());
+            self.scenery.push(
+                entities::Line {
+                    line_type,
+                    x1,
+                    y1,
+                    x2,
+                    y2,
+                }
+                .encode(),
+            );
         }
     }
 
     // Insert diagonal stripes
     pub fn insert_stripes(&mut self, size: i32, separation: i32, x: i32, y: i32, line_type: char) {
         // top/left
-        for i in 0..size*2 {
+        for i in 0..size * 2 {
             if i % separation == 0 {
                 let x1 = x - size;
                 let y1 = y + (size - 1 - i);
@@ -31,15 +49,33 @@ impl Track {
                 let y2 = y + size;
 
                 if line_type == 'p' {
-                    self.physical.push(entities::Line{ line_type, x1, y1, x2, y2 }.encode());
+                    self.physical.push(
+                        entities::Line {
+                            line_type,
+                            x1,
+                            y1,
+                            x2,
+                            y2,
+                        }
+                        .encode(),
+                    );
                 } else {
-                    self.scenery.push(entities::Line{ line_type, x1, y1, x2, y2 }.encode());
+                    self.scenery.push(
+                        entities::Line {
+                            line_type,
+                            x1,
+                            y1,
+                            x2,
+                            y2,
+                        }
+                        .encode(),
+                    );
                 }
             }
         }
 
         // bottom/right
-        for i in 0..(size*2)-1 {
+        for i in 0..(size * 2) - 1 {
             if i % separation == 0 {
                 let x1 = x + size;
                 let y1 = y - (size - 1 - i);
@@ -47,9 +83,27 @@ impl Track {
                 let y2 = y - size;
 
                 if line_type == 'p' {
-                    self.physical.push(entities::Line{ line_type, x1, y1, x2, y2 }.encode());
+                    self.physical.push(
+                        entities::Line {
+                            line_type,
+                            x1,
+                            y1,
+                            x2,
+                            y2,
+                        }
+                        .encode(),
+                    );
                 } else {
-                    self.scenery.push(entities::Line{ line_type, x1, y1, x2, y2 }.encode());
+                    self.scenery.push(
+                        entities::Line {
+                            line_type,
+                            x1,
+                            y1,
+                            x2,
+                            y2,
+                        }
+                        .encode(),
+                    );
                 }
             }
         }
@@ -60,34 +114,75 @@ impl Track {
         self.insert_stripes(size, 1, x, y, line_type);
     }
 
-
     // Powerups
     pub fn insert_check(&mut self, x: i32, y: i32) {
-        self.powerups += &entities::Powerup { powerup_type: 'C', x, y, rotation: 999 }.encode();
+        self.powerups += &entities::Powerup {
+            powerup_type: 'C',
+            x,
+            y,
+            rotation: 999,
+        }
+        .encode();
     }
 
     pub fn insert_star(&mut self, x: i32, y: i32) {
-        self.powerups += &entities::Powerup { powerup_type: 'T', x, y, rotation: 999 }.encode();
+        self.powerups += &entities::Powerup {
+            powerup_type: 'T',
+            x,
+            y,
+            rotation: 999,
+        }
+        .encode();
     }
 
     pub fn insert_slow_mo(&mut self, x: i32, y: i32) {
-        self.powerups += &entities::Powerup { powerup_type: 'S', x, y, rotation: 999 }.encode();
+        self.powerups += &entities::Powerup {
+            powerup_type: 'S',
+            x,
+            y,
+            rotation: 999,
+        }
+        .encode();
     }
 
     pub fn insert_bomb(&mut self, x: i32, y: i32) {
-        self.powerups += &entities::Powerup { powerup_type: 'O', x, y, rotation: 999 }.encode();
+        self.powerups += &entities::Powerup {
+            powerup_type: 'O',
+            x,
+            y,
+            rotation: 999,
+        }
+        .encode();
     }
 
     pub fn insert_gravity(&mut self, x: i32, y: i32, rot: i32) {
-        self.powerups += &entities::Powerup { powerup_type: 'G', x, y, rotation: rot }.encode();
+        self.powerups += &entities::Powerup {
+            powerup_type: 'G',
+            x,
+            y,
+            rotation: rot,
+        }
+        .encode();
     }
 
     pub fn insert_boost(&mut self, x: i32, y: i32, rot: i32) {
-        self.powerups += &entities::Powerup { powerup_type: 'B', x, y, rotation: rot }.encode();
+        self.powerups += &entities::Powerup {
+            powerup_type: 'B',
+            x,
+            y,
+            rotation: rot,
+        }
+        .encode();
     }
 
     pub fn insert_anti_gravity(&mut self, x: i32, y: i32) {
-        self.powerups += &entities::Powerup { powerup_type: 'A', x, y, rotation: 999 }.encode();
+        self.powerups += &entities::Powerup {
+            powerup_type: 'A',
+            x,
+            y,
+            rotation: 999,
+        }
+        .encode();
     }
 
     pub fn insert_teleport(&mut self, x1: i32, y1: i32, x2: i32, y2: i32) {
@@ -96,19 +191,43 @@ impl Track {
 
     // Vehicles
     pub fn insert_helicopter(&mut self, x: i32, y: i32) {
-        self.powerups += &entities::Powerup { powerup_type: '1', x, y, rotation: 1000 }.encode();
+        self.powerups += &entities::Powerup {
+            powerup_type: '1',
+            x,
+            y,
+            rotation: 1000,
+        }
+        .encode();
     }
 
     pub fn insert_truck(&mut self, x: i32, y: i32) {
-        self.powerups += &entities::Powerup { powerup_type: '2', x, y, rotation: 1000 }.encode();
+        self.powerups += &entities::Powerup {
+            powerup_type: '2',
+            x,
+            y,
+            rotation: 1000,
+        }
+        .encode();
     }
 
     pub fn insert_balloon(&mut self, x: i32, y: i32) {
-        self.powerups += &entities::Powerup { powerup_type: '3', x, y, rotation: 1000 }.encode();
+        self.powerups += &entities::Powerup {
+            powerup_type: '3',
+            x,
+            y,
+            rotation: 1000,
+        }
+        .encode();
     }
 
     pub fn insert_blob(&mut self, x: i32, y: i32) {
-        self.powerups += &entities::Powerup { powerup_type: '4', x, y, rotation: 1000 }.encode();
+        self.powerups += &entities::Powerup {
+            powerup_type: '4',
+            x,
+            y,
+            rotation: 1000,
+        }
+        .encode();
     }
 
     // Track code generation
@@ -139,4 +258,8 @@ impl Track {
 
         return final_data;
     }
+}
+
+pub fn encode(target: i32) -> String {
+    encode::base32_encode(target)
 }
