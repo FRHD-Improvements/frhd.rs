0adb04f2886e5be8e5deb3223ceb97d9af24f3f2
gaetgu <gabriel.ethan@gmail.com>
2022-06-30 20:32:21 -0600

    Two new curve functions (quadratic and bezier)

--- src/entities.rs
+++ src/entities.rs
@@ -1,38 +1,86 @@
 use crate::encode;
 
-pub struct Powerup {
-    pub powerup_type: char,
+pub enum PowerupTypes {
+    Row(RotatedPowerup),
+    Pow(Powerup),
+    Veh(Vehicle),
+    Tp(Teleport),
+}
+
+#[derive(Clone, Copy)]
+pub struct RotatedPowerup {
+    powerup_type: char,
     pub x: i32,
     pub y: i32,
     pub rotation: i32,
 }
 
-impl Powerup {
-    pub fn encode(self) -> String {
-        if self.rotation == 999 {
-            return format!("{} {} {},", 
-                           self.powerup_type.to_string(), 
-                           encode::base32_encode(self.x), 
-                           encode::base32_encode(self.y * -1)
-            );
-        } else if self.rotation == 1000 {
-            return format!("V {} {} {} a,",
-                            encode::base32_encode(self.x),
-                            encode::base32_encode(self.y * -1),
-                            self.powerup_type.to_string()
-            );
+impl RotatedPowerup {
+    pub fn new(powerup_type: char, x: i32, y: i32, rotation: i32) -> Self {
+        Self {
+            powerup_type,
+            x,
+            y,
+            rotation,
         }
-        
-        return format!("{} {} {} {},", 
-                       self.powerup_type, 
-                       encode::base32_encode(self.x), 
-                       encode::base32_encode(self.y * -1), 
-                       encode::base32_encode(self.rotation)
-        );
+    }
+
+    pub fn encode(self) -> String {
+        format!(
+            "{} {} {} {}",
+            self.powerup_type,
+            encode::base32_encode(self.x),
+            encode::base32_encode(self.y * -1),
+            encode::base32_encode(self.rotation)
+        )
     }
 }
 
+#[derive(Clone, Copy)]
+pub struct Powerup {
+    powerup_type: char,
+    x: i32,
+    y: i32,
+}
 
+impl Powerup {
+    pub fn new(powerup_type: char, x: i32, y: i32) -> Self {
+        Self { powerup_type, x, y }
+    }
+
+    pub fn encode(self) -> String {
+        format!(
+            "{} {} {}",
+            self.powerup_type,
+            encode::base32_encode(self.x),
+            encode::base32_encode(self.y)
+        )
+    }
+}
+
+#[derive(Clone, Copy)]
+pub struct Vehicle {
+    vehicle_type: char,
+    pub x: i32,
+    pub y: i32,
+}
+
+impl Vehicle {
+    pub fn new(vehicle_type: char, x: i32, y: i32) -> Self {
+        Self { vehicle_type, x, y }
+    }
+
+    pub fn encode(self) -> String {
+        format!(
+            "V {} {} {} a",
+            self.vehicle_type,
+            encode::base32_encode(self.x),
+            encode::base32_encode(self.y)
+        )
+    }
+}
+
+#[derive(Clone, Copy)]
 pub struct Teleport {
     pub x1: i32,
     pub y1: i32,
@@ -41,17 +89,22 @@ pub struct Teleport {
 }
 
 impl Teleport {
+    pub fn new(x1: i32, y1: i32, x2: i32, y2: i32) -> Self {
+        Self { x1, y1, x2, y2 }
+    }
+
     pub fn encode(self) -> String {
-        format!("W {} {} {} {}",
-                encode::base32_encode(self.x1),
-                encode::base32_encode(self.y1 * -1),
-                encode::base32_encode(self.x2),
-                encode::base32_encode(self.y2 * -1)
+        format!(
+            "W {} {} {} {}",
+            encode::base32_encode(self.x1),
+            encode::base32_encode(self.y1 * -1),
+            encode::base32_encode(self.x2),
+            encode::base32_encode(self.y2 * -1)
         )
     }
 }
 
-
+#[derive(Clone, Copy)]
 pub struct Line {
     pub line_type: char,
     pub x1: i32,
@@ -61,12 +114,23 @@ pub struct Line {
 }
 
 impl Line {
+    pub fn new(line_type: char, x1: i32, y1: i32, x2: i32, y2: i32) -> Self {
+        Self {
+            line_type,
+            x1,
+            y1,
+            x2,
+            y2,
+        }
+    }
+
     pub fn encode(self) -> String {
-        return format!("{} {} {} {},",
-                        encode::base32_encode(self.x1),
-                        encode::base32_encode(self.y1 * -1),
-                        encode::base32_encode(self.x2),
-                        encode::base32_encode(self.y2 * -1)
+        return format!(
+            "{} {} {} {}",
+            encode::base32_encode(self.x1),
+            encode::base32_encode(self.y1 * -1),
+            encode::base32_encode(self.x2),
+            encode::base32_encode(self.y2 * -1)
         );
     }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,21 +1,18 @@
+use entities::{Powerup, PowerupTypes, RotatedPowerup, Teleport, Vehicle};
+
 mod encode;
 mod entities;
 
-// TODO: Use vectors of types for the physical, scenery, and powerups fields. (Encode in the named step, not everytime something is added)
 pub struct Track {
-    pub trackdata: String,
-    pub physical: Vec<String>,
-    pub scenery: Vec<String>,
-    pub powerups: String,
+    pub lines: Vec<entities::Line>,
+    pub powerups: Vec<entities::PowerupTypes>,
 }
 
 impl Default for Track {
     fn default() -> Self {
         Self {
-            trackdata: String::new(),
-            physical: vec![],
-            scenery: vec![],
-            powerups: String::new(),
+            lines: vec![],
+            powerups: vec![],
         }
     }
 }
@@ -23,37 +20,14 @@ impl Default for Track {
 impl Track {
     // Merge another track into this one
     pub fn merge(&mut self, track: &mut Self) {
-        self.trackdata.push_str(&track.trackdata);
-        self.physical.append(&mut track.physical);
-        self.scenery.append(&mut track.scenery);
-        self.powerups.push_str(&track.powerups);
+        self.lines.append(&mut track.lines);
+        self.powerups.append(&mut track.powerups);
     }
 
     // Straight Line
     pub fn insert_line(&mut self, x1: i32, y1: i32, x2: i32, y2: i32, line_type: char) {
-        if line_type == 'p' {
-            self.physical.push(
-                entities::Line {
-                    line_type,
-                    x1,
-                    y1,
-                    x2,
-                    y2,
-                }
-                .encode(),
-            );
-        } else {
-            self.scenery.push(
-                entities::Line {
-                    line_type,
-                    x1,
-                    y1,
-                    x2,
-                    y2,
-                }
-                .encode(),
-            );
-        }
+        self.lines
+            .push(entities::Line::new(line_type, x1, y1, x2, y2));
     }
 
     // Quadratic Bezier Curve
@@ -67,207 +41,202 @@ impl Track {
         let mut points: Vec<[i32; 2]> = vec![];
 
         for i in 0..10 {
-            let t: f32 = i as f32 / 10.0;
-            let mut point: [i32; 2] = [0; 2];
+            let t = i as f32 / 10.0;
 
-            point[0] = ((1.0 - t).powf(2.0) * p0[0] as f32
-                + 2.0 * (1.0 - t) * t * p1[0] as f32
-                + t.powf(2.0) * p2[0] as f32) as i32;
+            let mut control_point_1: [f32; 2] = p0.map(|x| x as f32 * (1.0 - t));
+            control_point_1[0] += t * p1[0] as f32;
+            control_point_1[1] += t * p1[1] as f32;
 
-            point[1] = ((1.0 - t).powf(2.0) * p0[1] as f32
-                + 2.0 * (1.0 - t) * t * p1[1] as f32
-                + t.powf(2.0) * p2[1] as f32) as i32;
+            let mut control_point_2: [f32; 2] = p1.map(|x| x as f32 * (1.0 - t));
+            control_point_2[0] += t * p2[0] as f32;
+            control_point_2[1] += t * p2[1] as f32;
 
-            points.push(point);
+            let mut main_control_point_float: [f32; 2] = control_point_1.map(|x| x * (1.0 - t));
+            main_control_point_float[0] += t * control_point_2[0];
+            main_control_point_float[1] += t * control_point_2[1];
+
+            let main_control_point: [i32; 2] = [
+                main_control_point_float[0].round() as i32,
+                main_control_point_float[1].round() as i32,
+            ];
+
+            points.push(main_control_point);
         }
 
-        let mut first_index = 0;
-
         for i in 1..points.len() - 1 {
-            if (points[first_index][1] - points[i][1] < 2
-                && points[first_index][1] - points[i][1] > -2)
-                || (points[first_index][0] - points[i][0] < 2
-                    && points[first_index][0] - points[i][0] > -2)
-            {
-                break;
-            }
-
             self.insert_line(
-                points[first_index][0],
-                points[first_index][1],
+                points[i - 1][0],
+                points[i - 1][1],
                 points[i][0],
                 points[i][1],
                 line_type,
             );
-            first_index += 1;
+        }
+    }
+
+    // Cubic Bezier Curve
+    pub fn insert_cubic_curve(
+        &mut self,
+        p0: [i32; 2],
+        c0: [i32; 2],
+        c1: [i32; 2],
+        p1: [i32; 2],
+        line_type: char,
+    ) {
+        let mut points: Vec<[i32; 2]> = vec![];
+
+        let accuracy = 35;
+
+        for i in 0..accuracy {
+            let t = i as f32 / accuracy as f32;
+
+            // -- Nesting Level One
+            let mut control_point_1: [f32; 2] = p0.map(|x| x as f32 * (1.0 - t));
+            control_point_1[0] += t * c0[0] as f32;
+            control_point_1[1] += t * c0[1] as f32;
+
+            let mut control_point_2: [f32; 2] = c0.map(|x| x as f32 * (1.0 - t));
+            control_point_2[0] += t * c1[0] as f32;
+            control_point_2[1] += t * c1[1] as f32;
+
+            let mut control_point_3: [f32; 2] = c1.map(|x| x as f32 * (1.0 - t));
+            control_point_3[0] += t * p1[0] as f32;
+            control_point_3[1] += t * p1[1] as f32;
+
+            // -- Nesting Level Two
+            let mut control_point_4: [f32; 2] = control_point_1.map(|x| x * (1.0 - t));
+            control_point_4[0] += t * control_point_2[0];
+            control_point_4[1] += t * control_point_2[1];
+
+            let mut control_point_5: [f32; 2] = control_point_2.map(|x| x * (1.0 - t));
+            control_point_5[0] += t * control_point_3[0];
+            control_point_5[1] += t * control_point_3[1];
+
+            // -- Nesting Level Three (last level)
+            let mut main_control_point_float: [f32; 2] = control_point_4.map(|x| x * (1.0 - t));
+            main_control_point_float[0] += t * control_point_5[0];
+            main_control_point_float[1] += t * control_point_5[1];
+
+            let main_control_point: [i32; 2] = [
+                main_control_point_float[0].round() as i32,
+                main_control_point_float[1].round() as i32,
+            ];
+
+            points.push(main_control_point);
+        }
+
+        for i in 1..points.len() - 1 {
+            self.insert_line(
+                points[i - 1][0],
+                points[i - 1][1],
+                points[i][0],
+                points[i][1],
+                line_type,
+            );
         }
     }
 
     // Powerups
     pub fn insert_check(&mut self, x: i32, y: i32) {
-        self.powerups += &entities::Powerup {
-            powerup_type: 'C',
-            x,
-            y,
-            rotation: 999,
-        }
-        .encode();
+        self.powerups
+            .push(PowerupTypes::Pow(Powerup::new('C', x, y)));
     }
 
     pub fn insert_star(&mut self, x: i32, y: i32) {
-        self.powerups += &entities::Powerup {
-            powerup_type: 'T',
-            x,
-            y,
-            rotation: 999,
-        }
-        .encode();
+        self.powerups
+            .push(PowerupTypes::Pow(Powerup::new('T', x, y)));
     }
 
     pub fn insert_slow_mo(&mut self, x: i32, y: i32) {
-        self.powerups += &entities::Powerup {
-            powerup_type: 'S',
-            x,
-            y,
-            rotation: 999,
-        }
-        .encode();
+        self.powerups
+            .push(PowerupTypes::Pow(Powerup::new('S', x, y)));
     }
 
     pub fn insert_bomb(&mut self, x: i32, y: i32) {
-        self.powerups += &entities::Powerup {
-            powerup_type: 'O',
-            x,
-            y,
-            rotation: 999,
-        }
-        .encode();
+        self.powerups
+            .push(PowerupTypes::Pow(Powerup::new('O', x, y)));
     }
 
     pub fn insert_gravity(&mut self, x: i32, y: i32, rot: i32) {
-        self.powerups += &entities::Powerup {
-            powerup_type: 'G',
-            x,
-            y,
-            rotation: rot,
-        }
-        .encode();
+        self.powerups
+            .push(PowerupTypes::Row(RotatedPowerup::new('G', x, y, rot)));
     }
 
     pub fn insert_boost(&mut self, x: i32, y: i32, rot: i32) {
-        self.powerups += &entities::Powerup {
-            powerup_type: 'B',
-            x,
-            y,
-            rotation: rot,
-        }
-        .encode();
+        self.powerups
+            .push(PowerupTypes::Row(RotatedPowerup::new('B', x, y, rot)));
     }
 
     pub fn insert_anti_gravity(&mut self, x: i32, y: i32) {
-        self.powerups += &entities::Powerup {
-            powerup_type: 'A',
-            x,
-            y,
-            rotation: 999,
-        }
-        .encode();
+        self.powerups
+            .push(PowerupTypes::Pow(Powerup::new('A', x, y)));
     }
 
     pub fn insert_teleport(&mut self, x1: i32, y1: i32, x2: i32, y2: i32) {
-        self.powerups += &entities::Teleport { x1, y1, x2, y2 }.encode();
+        self.powerups
+            .push(PowerupTypes::Tp(Teleport::new(x1, y1, x2, y2)));
     }
 
     // Vehicles
     pub fn insert_helicopter(&mut self, x: i32, y: i32) {
-        self.powerups += &entities::Powerup {
-            powerup_type: '1',
-            x,
-            y,
-            rotation: 1000,
-        }
-        .encode();
+        self.powerups
+            .push(PowerupTypes::Veh(Vehicle::new('1', x, y)));
     }
 
     pub fn insert_truck(&mut self, x: i32, y: i32) {
-        self.powerups += &entities::Powerup {
-            powerup_type: '2',
-            x,
-            y,
-            rotation: 1000,
-        }
-        .encode();
+        self.powerups
+            .push(PowerupTypes::Veh(Vehicle::new('2', x, y)));
     }
 
     pub fn insert_balloon(&mut self, x: i32, y: i32) {
-        self.powerups += &entities::Powerup {
-            powerup_type: '3',
-            x,
-            y,
-            rotation: 1000,
-        }
-        .encode();
+        self.powerups
+            .push(PowerupTypes::Veh(Vehicle::new('3', x, y)));
     }
 
     pub fn insert_blob(&mut self, x: i32, y: i32) {
-        self.powerups += &entities::Powerup {
-            powerup_type: '4',
-            x,
-            y,
-            rotation: 1000,
-        }
-        .encode();
+        self.powerups
+            .push(PowerupTypes::Veh(Vehicle::new('4', x, y)));
     }
 
     // Clear all of the information in the track
     pub fn clear(&mut self) {
-        self.trackdata = String::new();
-        self.physical = vec![];
-        self.scenery = vec![];
-        self.powerups = String::new();
+        self.lines = vec![];
+        self.powerups = vec![];
     }
 
     // Track code generation
     pub fn generate_code(&mut self) -> String {
-        for physical_line in &self.physical {
-            self.trackdata += physical_line;
+        let mut physical_data: Vec<String> = vec![];
+        let mut scenery_data: Vec<String> = vec![];
+        let mut powerup_data: Vec<String> = vec![];
+
+        for line in &self.lines {
+            if line.line_type == 'p' {
+                physical_data.push(line.encode());
+            } else {
+                scenery_data.push(line.encode());
+            }
         }
 
-        for scenery_line in &self.scenery {
-            self.trackdata += scenery_line;
+        for powerup in &self.powerups {
+            match powerup {
+                PowerupTypes::Row(a) => powerup_data.push(a.encode()),
+                PowerupTypes::Pow(a) => powerup_data.push(a.encode()),
+                PowerupTypes::Veh(a) => powerup_data.push(a.encode()),
+                PowerupTypes::Tp(a) => powerup_data.push(a.encode()),
+            }
         }
 
-        let mut final_data = String::new();
-
-        for line in &self.physical {
-            final_data += line;
-        }
-
-        final_data += "#";
-
-        for line in &self.scenery {
-            final_data += line;
-        }
-
-        final_data += "#";
-        final_data += &self.powerups;
-        final_data += "#";
-
-        return final_data;
+        format!(
+            "{}#{}#{}",
+            physical_data.join(","),
+            scenery_data.join(","),
+            powerup_data.join(",")
+        )
     }
 }
 
-// I might as well provide a method to simply encode numbers
+// I might as well provide a public method to simply encode numbers
 pub fn encode(target: i32) -> String {
     encode::base32_encode(target)
-}
-
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn test_merge() {
-        let mut t_a: crate::Track = Default::default();
-        let mut t_b: crate::Track = Default::default();
-        t_a.merge(&mut t_b);
-    }
 }
